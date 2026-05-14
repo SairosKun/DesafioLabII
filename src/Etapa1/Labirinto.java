66d7a35889923c96cf9e6d0d660da9ca46b93a7f
SairosKun <niko.12@hotmail.com>
2026-05-14 18:10:10 -0300

    primeiro commit

--- src/Etapa1/Labirinto.java
+++ src/Etapa1/Labirinto.java
@@ -1,6 +1,30 @@
-import java.util.*;
-import java.io.*;
+import java.io.BufferedReader;
+import java.io.FileReader;
+import java.io.IOException;
+import java.util.ArrayList;
+import java.util.List;
 
 public class Labirinto {
-    
+    private static final char PAREDE = 'X';
+    private static final char CAMINHO = ' ';
+    private static final char INICIO = 'D';
+    private static final char FIM = '#';
+
+    private int linhas;
+    private int colunas;
+    private char[][] labirinto;
+    private boolean solucaoEncontrada = false;
+
+    public void criaLabirinto(String filename){
+        List<String> linhasList = new ArrayList<>();
+        try(BufferedReader br = new BufferedReader(new FileReader(filename))) {
+            String linha;
+            while ((linha = br.readLine()) != null) {
+                linhasList.add(linha);
+            }
+        } catch (IOException e) {
+            e.printStackTrace();
+        }
+    }
+
 }
